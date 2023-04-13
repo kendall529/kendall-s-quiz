9e3cc739a25dadbaab70ba08e2c59a9f651fa20b
kendall529 <kendall.smith529@gmail.com>
2023-04-13 18:24:04 -0500

    JS updates

--- assests/config.js
+++ assests/config.js
@@ -1,9 +1,17 @@
-var start = document.querySelector('#start')
-var score = document.querySelector('#score')
-var timerEl = document.querySelector('#countdown')
+var start = document.querySelector('#start');
+var score = document.querySelector('#score');
+var timerEl = document.querySelector('#countdown');
+var cards = document.querySelector('.card');
+// var currentCard = cards.dataset.status('active');
+var button = document.querySelector('.btn');
+var answerCorrect = document.querySelector('.correct');
+var answerIncorrect = document.querySelector('.incorrect');
+var hidden = document.querySelector('.content');
+var delay = 1000;
 
 
 start.addEventListener('click', function() {
+    startGame();
     var timeLeft = 30;
 
     var interval = setInterval(function () {
@@ -17,25 +25,26 @@ start.addEventListener('click', function() {
     }, 1000);
 })
 
+function startGame() {
+    start.setAttribute('style', 'display: none;');
+    hidden.classList.remove('hide');
+}
 
 
-var activeIndex = 0;
 
-var cards = document.getElementsByClassName('card');
-
-var button = document.querySelector('.btn');
-var answerCorrect = document.querySelector('.correct');
-var answerIncorrect = document.querySelector('.incorrect');
 
 function nextQuestion() {
-    var nextIndex = activeIndex + 1 <= cards.length;
+    // if(cards.dataset.status = 'active' && cards.dataset.index == [0]) {
+        // for(var i = 0; i < cards.length; i++)
+        // cards.dataset.index = [1];
+        // cards.dataset.status = 'active';
+        // return;
+    // }
+    if(cards.dataset.index = [0]) {
+        // cards.setAttribute('style', 'scale: 0;');
+        cards.setAttribute('data-status', 'unknown');
+    }
 
-    var currentCard = document.querySelector(`[data-index="${activeIndex}"]`),
-    nextCard = document.querySelector(`[data-index="${nextIndex}"]`);
-
-    currentCard.dataset.status = 'unknown';
-
-    nextCard.dataset.status = 'active';
 }
 
 button.addEventListener('click', function(event) {
@@ -46,16 +55,23 @@ button.addEventListener('click', function(event) {
 
         if(buttonState === 'correct') {
             answerCorrect.textContent = answerCorrect.dataset.string;
+            score.textContent = 10;
 
-            answerCorrect.dataset.state = 'visible';
-        } else {
-            answerIncorrect.textContent = answerIncorrect.dataset.string;
+            // answerCorrect.dataset.state = 'visible';
+        } 
+        if(buttonState !== 'correct') {
 
-            answerIncorrect.dataset.state = 'visible';
+            console.log('hello')
+            // answerIncorrect.textContent = answerIncorrect.dataset.string;
+
+            // answerIncorrect.dataset.state = 'visible';
         }
-    nextQuestion();
+    // nextQuestion();
+    setTimeout(nextQuestion, delay);
     }
 }) 
+
+
 
 // function renderScore() {
 //     score.textContent = 0;
@@ -69,3 +85,6 @@ button.addEventListener('click', function(event) {
 // upon clicking an answer: the index will increase by 1 on the card class divs
 // the active index will become dataset.status = 'unknown'
 // the next index will become dataset.status = 'active'
+
+// for(var i = 0; i < cards.length; i++);
+
